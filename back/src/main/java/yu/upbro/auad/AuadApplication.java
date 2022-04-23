cf1f107d9288d258c064eebb22fa873049cfbce7
오정환 <hwaning1>
2022-04-23 23:28:33 +0900

    first commit

---
+++ back/src/main/java/yu/upbro/auad/AuadApplication.java
@@ -0,0 +1,13 @@
+package yu.upbro.auad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuadApplication.class, args);
+	}
+
+}
