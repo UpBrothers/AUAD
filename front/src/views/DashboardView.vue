dffde10a2dd1a951b7736a559ff9af55ee8aa130
Kim Hyeon <hye07on11@gmail.com>
2022-04-25 01:42:11 +0900

    add DashboardView

---
+++ front/src/views/DashboardView.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="3">
+        <v-sheet rounded="lg" min-height="268" class="sticky">
+          <!--  -->
+        </v-sheet>
+      </v-col>
+
+      <v-col cols="12" sm="6">
+        <PostCard v-for="n in 4" :class="n > 1 ? 'mt-4' : ''" :key="n" />
+      </v-col>
+
+      <v-col cols="12" sm="3">
+        <v-sheet rounded="lg" min-height="268" class="sticky">
+          <v-calendar ref="calendar" style="height: 300px"></v-calendar>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import PostCard from "../components/PostCard";
+
+export default {
+  name: "DashboardView",
+
+  components: {
+    PostCard,
+  },
+
+  data: () => ({}),
+};
+</script>
+
+<style scoped>
+.sticky {
+  position: sticky;
+  top: calc(50vh - 134px);
+}
+</style>
